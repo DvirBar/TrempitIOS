e6d31cb50a9e976f9db0d250918c90fbc54999e7
Dvir Bartov <dvir.bartov1@gmail.com>
2023-10-01 01:26:08 +0300

    initial commit

---
+++ Trempit/TrempitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrempitApp.swift
+//  Trempit
+//
+//  Created by Dvir Bartov on 30/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct TrempitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
